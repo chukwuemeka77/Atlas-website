f980f3ebec3973617bf69387b25113265cec070d
Chukwuemeka <101349109+chukwuemeka77@users.noreply.github.com>
2025-11-25 10:20:50 +0000

    Update tailwind.config.js

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,16 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{ts,tsx}"
-  ],
+module.exports = {
+  darkMode: "class",
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        atlasBlue: "#1E40AF",
-        atlasPink: "#EC4899"
-      }
-    }
+    extend: {},
   },
-  plugins: []
+  plugins: [],
 };
